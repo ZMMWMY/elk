ecfc7ca60a9a2dad6a3f44c1e1228eef87e71eed
ZMMWMY <zhouqiming@songxiaocai.com>
2018-04-23 17:29:46 +0800

    user and role yml

---
+++ elastic/bin/init-sgadmin.sh
@@ -0,0 +1,2 @@
+ sh /usr/share/elasticsearch/plugins/search-guard-6/tools/sgadmin.sh -tspass FwSF3LrWcRxmMpYa -kspass 8JTbhLdhslmzWEL3 -cd /usr/share/elasticsearch/config/sg
+ -ks /usr/share/elasticsearch/config/sg/sgadmin-keystore.jks  -ts /usr/share/elasticsearch/config/sg/truststore.jks -nhnv -icl
